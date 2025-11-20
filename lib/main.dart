c006e1b7f378933ac9269e398462fea492969f8d
amirulmukmin3007 <amirulmukmin878@gmail.com>
2025-11-20 12:07:28 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,187 +1,216 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Basic Widgets Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
       home: Scaffold(
+        backgroundColor: Color(0xFFc0d4ff),
         // Status Bar and App Bar area
         appBar: AppBar(
-          backgroundColor: Colors.yellow,
-          title: Text('Home'),
+          backgroundColor: Color(0xFF012169),
+          title: Text('Home', style: TextStyle(color: Colors.white)),
           actions: [
             Padding(
               padding: const EdgeInsets.fromLTRB(20, 5, 10, 0),
-              child: Image.asset('assets/ke_logo.png', width: 30),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Image.asset('assets/zen_logo.png', width: 30),
+              ),
             ),
           ],
         ),
 
         // Main content area - Safe Area
-        body: Center(
+        body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Kotak "Welcome, User!"
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  width: double.infinity,
-                  height: 100,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Welcome, User!',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Icon(Icons.account_circle),
-                      ],
-                    ),
-                  ),
+                // Text "Welcome, User!"
+                Text(
+                  'Welcome,',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
+                Text('Muhammad Amirul Mukmin,', style: TextStyle(fontSize: 16)),
 
                 SizedBox(height: 20),
 
                 // Kotak "Task List"
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[800],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  width: double.infinity,
-                  height: 500,
-                  child: DefaultTabController(
-                    length: 2,
-                    child: Column(
-                      children: [
-                        TabBar(
-                          labelColor: Colors.yellow,
-                          unselectedLabelColor: Colors.white,
-                          indicatorColor: Colors.yellow,
-                          indicatorSize: TabBarIndicatorSize.tab,
-                          tabs: [
-                            Tab(text: 'To Do'),
-                            Tab(text: 'Missed'),
-                          ],
-                        ),
-
-                        Expanded(
-                          child: TabBarView(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
                             children: [
-                              ListView(
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.all(5.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.yellow,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: ListTile(
-                                      title: Text(
-                                        'Kedai Makan ABC',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      subtitle: Text('20/5/2025'),
-                                      trailing: Text(
-                                        'RM 20.00',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.yellow,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: ListTile(
-                                      title: Text(
-                                        'Kedai Makan ABC',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      subtitle: Text('20/5/2025'),
-                                      trailing: Text(
-                                        'RM 20.00',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                              Icon(
+                                Icons.access_time_rounded,
+                                color: Colors.orange,
                               ),
-                              ListView(
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.all(5.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.yellow,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: ListTile(
-                                      title: Text(
-                                        'Kedai Makan ABC',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      subtitle: Text('20/5/2025'),
-                                      trailing: Text(
-                                        'RM 20.00',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: EdgeInsets.all(5.0),
-                                    decoration: BoxDecoration(
-                                      color: Colors.yellow,
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: ListTile(
-                                      title: Text(
-                                        'Kedai Makan ABC',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      subtitle: Text('20/5/2025'),
-                                      trailing: Text(
-                                        'RM 20.00',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                ],
+                              const SizedBox(width: 5),
+                              Text(
+                                'Ongoing',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          Text(
+                            '15',
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.orange,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.done, color: Colors.green),
+                              const SizedBox(width: 5),
+                              Text(
+                                'Done',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            '25',
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.warning_amber, color: Colors.red),
+                              const SizedBox(width: 5),
+                              Text(
+                                'Due',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            '3',
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 150,
+                      height: 150,
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.lightbulb, color: Colors.purple),
+                              const SizedBox(width: 5),
+                              Text(
+                                'Proposed',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            '8',
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -190,8 +219,8 @@ class MyApp extends StatelessWidget {
 
         // Navigation Bar
         bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.black,
-          selectedItemColor: Colors.white,
+          backgroundColor: Colors.white,
+          selectedItemColor: Color(0xFF012169),
           unselectedItemColor: Colors.grey,
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
